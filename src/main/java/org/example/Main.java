a24674951a0ac72f508fabc4906c9e4c8bad8f79
Tomasz Marchela <bingor1@wp.pl>
2025-07-01 22:50:09 +0200

    Builder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import org.example.constructors.NutritionFacts;
 import org.example.constructors.Temperature;
 
 public class Main {
     public static void main(String[] args) {
+        tempTest();
+    }
+    public static void tempTest(){
         Temperature temperature = Temperature.fromCelcius(32);
         Temperature temperature2 = Temperature.fromFahrenheit(32);
         Temperature temperature3 = Temperature.fromKelvin(300);
@@ -12,5 +16,11 @@ public class Main {
         System.out.println(temperature3.toFahrenheit());
         System.out.println(temperature3.toKelvin());
         System.out.println(temperature3.toCelcius());
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(240)
+                .sodium(35)
+                .carbohydrate(40)
+                .build();
+        System.out.println(cocaCola);
     }
 }
